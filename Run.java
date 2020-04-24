cf99a920cd112928b9212eedf4434a1fa17984f6
droiddoes9 <52133386+droiddoes9@users.noreply.github.com>
2020-04-24 15:17:51 -0500

    ABOUT TO MAKE SCARY CHANGES, REVERT TO THIS IF NEEDED

--- Run.java
+++ Run.java
@@ -3,8 +3,27 @@ import java.util.ArrayList;
 public class Run {
 
     public static void main(String [] args) {
+        int [] nums = {4, 5, 7, 11, 9, 13, 10, 8, 12};
+        int target = 20;
 
-        String str = "mary had a little lamb";
+        for (int i = 0; i < nums.length; i++){
+            int num1 = nums[i];
+            for (int j = 0; j < i; j++){
+                int num2 = nums[j];
+
+                if (num1 + num2 == target){
+                    if (num1 > num2){
+                        System.out.println(num1 + ", " + num2);
+                    }
+
+                    else{
+                        System.out.println(num2 + ", " + num1);
+                    }
+                }
+            }
+        }        
 
     }
+
+
 }
